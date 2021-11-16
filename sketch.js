479366b26284a026c11c493507cde1aeb660e123
antonysiby <91169646+antonysiby@users.noreply.github.com>
2021-11-16 08:49:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var weight = [43,38,30,45,29];
+console.log(weight)
+
+function setup() 
+{
+  createCanvas(400,400);
+  var sum = weight[0] + weight[1] + weight[2] + weight[3] + weight[4] 
+var avg = sum/weight.length
+console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
